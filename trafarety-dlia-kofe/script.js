bd1f6151ee2d763312c1f6610393365ba65a9cda
vassiliy278 <vasyl.koval@pm.bet>
2022-02-15 14:26:31 +0200

    ok

--- trafarety-dlia-kofe/script.js
+++ trafarety-dlia-kofe/script.js
@@ -1,10 +1,10 @@
 const img = document.querySelector(".main_img");
 const imgArr = [
-  "/img/coffee_01.jpeg",
-  "/img/coffee_02.jpeg",
-  "/img/coffee_03.jpeg",
-  "/img/coffee_04.jpeg",
-  "/img/coffee_07.jpeg",
+  "./img/coffee_01.jpeg",
+  "./img/coffee_02.jpeg",
+  "./img/coffee_03.jpeg",
+  "./img/coffee_04.jpeg",
+  "./img/coffee_07.jpeg",
 ];
 
 setInterval(function () {
